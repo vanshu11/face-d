f0234d31b589fd4ba864efdbca03d7fb7638b8d0
vanshu11 <87985098+vanshu11@users.noreply.github.com>
2021-10-16 15:57:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+Webcam.attach("#camera");
+
+camera = document.getElementById("camera");
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="image_captured" src="'+data_uri+'"/>';
+    });
+};
+
+console.log('ml5 Version:',ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/dSYIe0iqQ/,modelLoaded');
+
+function modelLoaded() {
+    console.log("Model Loaded Successfully!");
+};
+
+function check() {
+    img = document.getElementById("image_captured");
+    classifier.classify(img, gotResults);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_of_objects").innerHTML = results[0].label;
+        document.getElementById("result_of_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
+
